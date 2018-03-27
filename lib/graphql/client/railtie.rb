054257e4c8a036c85f875980ec60192d1d84d1e2
Takuma Osada <judgment9ttk069@gmail.com>
2018-03-27 15:56:58 +0900

    WIP: Make GraphQL Client work for Rails 5 projects

--- lib/graphql/client/railtie.rb
+++ lib/graphql/client/railtie.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 require "graphql"
 require "graphql/client"
+require "graphql/client/controller_helpers"
 require "rails/railtie"
 
 module GraphQL
@@ -14,7 +15,6 @@ module GraphQL
     #
     class Railtie < Rails::Railtie
       config.graphql = ActiveSupport::OrderedOptions.new
-      config.graphql.client = GraphQL::Client.new
 
       initializer "graphql.configure_log_subscriber" do |_app|
         require "graphql/client/log_subscriber"
@@ -22,26 +22,30 @@ module GraphQL
       end
 
       initializer "graphql.configure_erb_implementation" do |_app|
-        require "graphql/client/erubis"
-        ActionView::Template::Handlers::ERB.erb_implementation = GraphQL::Client::Erubis
+        if Rails.version >= "5.1"
+          require "graphql/client/erubi"
+          ActionView::Template::Handlers::ERB.erb_implementation = GraphQL::Client::Erubi
+        else
+          require "graphql/client/erubis"
+          ActionView::Template::Handlers::ERB.erb_implementation = GraphQL::Client::Erubis
+        end
       end
+
+      initializer "graphql.configure_controller_helpers" do |_app|
+        ActiveSupport.on_load(:action_controller) do
+          include GraphQL::Client::ControllerHelpers
+        end
 
       initializer "graphql.configure_views_namespace" do |app|
         require "graphql/client/view_module"
 
         path = app.paths["app/views"].first
 
-        # TODO: Accessing config.graphql.client during the initialization
-        # process seems error prone. The application may reassign
-        # config.graphql.client after this block is executed.
-        client = config.graphql.client
-
         config.watchable_dirs[path] = [:erb]
 
         Object.const_set(:Views, Module.new do
           extend GraphQL::Client::ViewModule
           self.path = path
-          self.client = client
         end)
       end
     end
